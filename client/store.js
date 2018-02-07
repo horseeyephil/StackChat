66399ef06a65068e5c40471fabccb3bcfa354773
Noelle Laureano <nrlaureano@gmail.com>
2018-02-07 18:00:40 -0500

    The Store - complete

---
+++ client/store.js
@@ -0,0 +1,31 @@
+import { createStore } from 'redux';
+
+// ACTION TYPES
+const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+
+// ACTION CREATORS
+export function gotMessagesFromServer(messageArr) {
+  return {
+    type: GOT_MESSAGES_FROM_SERVER,
+    messages: messageArr
+  };
+}
+
+// INITIAL STATE
+const initialState = {
+  messages: []
+};
+
+// REDUCER
+const reducer = (prevState = initialState, action) => {
+  switch (action.type) {
+    case GOT_MESSAGES_FROM_SERVER:
+      return Object.assign({}, prevState, { messages: action.messages });
+    default:
+      return prevState;
+  }
+};
+
+// STORE
+const store = createStore(reducer);
+export default store;
